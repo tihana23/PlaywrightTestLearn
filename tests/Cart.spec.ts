e7f39c73e6ab8a42d3945f3aa719ae741f2960e2
tihana <tihana23@gmail.com>
2024-01-24 00:57:55 +0100

    cart added test

--- tests/Cart.spec.ts
+++ tests/Cart.spec.ts
@@ -250,3 +250,156 @@ test("Verify total in cart is calculated correctly", async ({ page }) => {
   console.log(`Expected Total: ${expectedTotal}, Actual Total: ${actualTotal}`);
   expect(expectedTotal).toBe(actualTotal);
 });
+test("Verify that Place order is working correcly..not so much but...", async () => {
+  const browser: Browser = await chromium.launch({ headless: false });
+  const page: Page = await browser.newPage();
+  await page.goto("https://www.demoblaze.com/", { waitUntil: "networkidle" });
+
+  const addProductToCart = async (productName: string) => {
+    await page.click(`.card-title a:has-text("${productName}")`);
+    const linkAddCart8 = page.getByText("Add to cart");
+    await linkAddCart8.click({ force: true });
+    await page.waitForTimeout(1000);
+    const locator = page.locator("a.navbar-brand#nava");
+
+    await locator.click();
+  };
+  await addProductToCart("Samsung galaxy s6");
+
+  await addProductToCart("Nokia lumia 1520");
+
+  await page.click("#cartur");
+
+  const getPriceFromCart = async (productName: string) => {
+    const productRow = page.locator(`tr.success:has-text("${productName}")`);
+
+    const priceText = await productRow.locator("td:nth-child(3)").textContent();
+    if (priceText !== null) {
+      return parseFloat(priceText);
+    } else {
+      console.error(`Price not found for ${productName}`);
+      return 0;
+    }
+  };
+  await page.waitForTimeout(1000);
+  const priceSamsung = await getPriceFromCart("Samsung galaxy s6");
+  const priceNokia = await getPriceFromCart("Nokia lumia 1520");
+  const expectedTotal = priceSamsung + priceNokia;
+  const totalText = await page.textContent("#totalp");
+  const actualTotal = totalText ? parseFloat(totalText) : 0;
+  console.log(`Expected Total: ${expectedTotal}, Actual Total: ${actualTotal}`);
+  expect(expectedTotal).toBe(actualTotal);
+  const buttonPlaceOrder = page.locator('button:has-text("Place Order")');
+  await buttonPlaceOrder.click();
+  const placeOrderNameTextbox = page.locator("#name");
+  const placeOrderCountryTextbox = page.locator("#country");
+  const placeOrderCityTextbox = page.locator("#city");
+  const placeOrderCreditCardTextbox = page.locator("#card");
+  const placeOrderMonthTextbox = page.locator("#month");
+  const placeOrderYearTextbox = page.locator("#year");
+
+  const firstNameUsed = faker.person.firstName();
+  await placeOrderNameTextbox.fill(firstNameUsed);
+  await placeOrderCountryTextbox.fill(faker.location.country());
+  await placeOrderCityTextbox.fill(faker.location.city());
+  await placeOrderCreditCardTextbox.fill(faker.finance.creditCardNumber());
+  await placeOrderMonthTextbox.fill(faker.date.month());
+  await placeOrderYearTextbox.fill(faker.date.month());
+
+  const buttonPurchase = page.locator('button[onclick="purchaseOrder()"]');
+  await page.waitForTimeout(5000);
+  await buttonPurchase.click();
+  await page.waitForTimeout(5000);
+  const thankYouMessageLocator = page.locator(
+    'h2:has-text("Thank you for your purchase!")'
+  );
+  const amountPopup = page.locator("p.lead.text-muted");
+  const namePopup = page.locator("p.lead.text-muted >> text=/Name: .+/");
+  await page.waitForTimeout(5000);
+  const tyMessageExpected = "Thank you for your purchase!";
+  const thankYouMessageTextActual = await thankYouMessageLocator.textContent();
+  expect(tyMessageExpected).toEqual(thankYouMessageTextActual);
+  const amountText = await amountPopup.textContent();
+  // Initialize a variable to hold the parsed amount
+  let amount = 0;
+  if (amountText !== null) {
+    const amountMatch = amountText.match(/Amount: (\d+) USD/);
+    if (amountMatch) {
+      amount = parseFloat(amountMatch[1]);
+    }
+  }
+  // not null
+  const nameText = await namePopup.textContent();
+  let name = "";
+
+  if (nameText !== null) {
+    const nameMatch = nameText.match(/Name: (.+?)(?=Date)/);
+    if (nameMatch) {
+      name = nameMatch[1].trim();
+    }
+  }
+
+  console.log(`Total Price: ${actualTotal}, Amount: ${amount}`);
+  console.log(`Form Name: ${firstNameUsed}, Name: ${name}`);
+  if (actualTotal === amount && firstNameUsed === name) {
+    console.log("Purchase details match.");
+  } else {
+    console.error("Mismatch in purchase details.");
+  }
+  expect(actualTotal).toBe(amount);
+  expect(firstNameUsed).toBe(name);
+});
+
+test("Verify that popup window appeared if data is not populated", async () => {
+  const browser: Browser = await chromium.launch({ headless: false });
+  const page: Page = await browser.newPage();
+
+  await page.goto("https://www.demoblaze.com/", { waitUntil: "networkidle" });
+
+  const addProductToCart = async (productName: string) => {
+    await page.click(`.card-title a:has-text("${productName}")`);
+    const linkAddCart8 = page.getByText("Add to cart");
+    await linkAddCart8.click({ force: true });
+    await page.waitForTimeout(1000);
+    const locator = page.locator("a.navbar-brand#nava");
+
+    await locator.click();
+  };
+  await addProductToCart("Samsung galaxy s6");
+
+  await addProductToCart("Nokia lumia 1520");
+
+  await page.click("#cartur");
+  const buttonPlaceOrder = page.locator('button:has-text("Place Order")');
+  await buttonPlaceOrder.click();
+  page.on("dialog", async (dialog) => {
+    const text = dialog.message();
+    console.log(text);
+    expect(dialog.message()).toBe("Please fill out Name and Creditcard.");
+    await dialog.accept();
+  });
+  const buttonPurchase = page.locator('button[onclick="purchaseOrder()"]');
+  await page.waitForTimeout(5000);
+  await buttonPurchase.click();
+});
+test("Verify that Close button inside place order working and Place order window is closed", async () => {
+  const browser: Browser = await chromium.launch({ headless: false });
+  const page: Page = await browser.newPage();
+
+  await page.goto("https://www.demoblaze.com/", { waitUntil: "networkidle" });
+
+  await page.click("#cartur");
+  const buttonPlaceOrder = page.locator('button:has-text("Place Order")');
+  await buttonPlaceOrder.click();
+  await expect(
+    page.locator('div.modal-header:has-text("Place order")')
+  ).toBeVisible();
+
+  const closeButtonLocator = page.locator(
+    "xpath=/html/body/div[3]/div/div/div[3]/button[1]"
+  );
+  await closeButtonLocator.click();
+  await expect(
+    page.locator('div.modal-header:has-text("Place order")')
+  ).not.toBeVisible();
+});
