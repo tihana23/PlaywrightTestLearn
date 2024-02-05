0ca28dafc5d69494045f05022d47bebec6bd39c7
tihana23 <tihana23@gmail.com>
2024-02-05 11:35:31 +0100

    Update ProductPageTest.spec.ts
    update verify product

--- tests/PageObjectTests/ProductPageTest.spec.ts
+++ tests/PageObjectTests/ProductPageTest.spec.ts
@@ -11,16 +11,21 @@ test("Verify that Product page have all fields visible", async ({ page }) => {
   await homePage.selectProduct("Samsung galaxy s6");
   await productPage.verifyProductIsOpenAndAllFieldsAreVisible(
   );
+  await expect(productPage.productName).toContain("Samsung galaxy s6");
 });
 
 test("Verify that Add to cart button is working and show popup message", async ({
   page,
 }) => {
   const homePage = new HomePage(page);
+  const cartPage = new CartPage(page);
+  const navigationBar = new NavigationBar(page);
   const productPage = new ProductPage(page, "Samsung galaxy s6");
   await homePage.goTo();
   await homePage.navigateToPhonesCategory();
   await homePage.selectProduct("Samsung galaxy s6");
   await popuUpMessage(page, "Product added");
-  await productPage.addProductToCart();
+ await productPage.addProductToCart();
+  await navigationBar.navigateToCart();
+  await cartPage.verifyProductInCart("Samsung galaxy s6"););
 });
